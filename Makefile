aa710be6279d008d9f3eb8f73325d0a944585eac
Cody <cody.reeth@gmail.com>
2026-08-08 16:24:16 -0400

    feat(C++): added c++ infrastructure

---
+++ Makefile
@@ -0,0 +1,2 @@
+SVD: svd.cc
+	g++ -o svd svd.cc
--- svd.py
+++ svd.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.sparse.linalg import svds
 import cv2
 
-video = cv2.VideoCapture("video.mov")
+video = cv2.VideoCapture("IMG_7857.MOV")
 
 A = []
 
@@ -16,7 +16,8 @@ while(video.isOpened()):
     cv2.imshow("Video", frame)
 
     #gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray_frame = cv2.resize(frame, (1028, 1028))
+    print(frame.shape)
+    gray_frame = cv2.resize(frame, (1920, 1080))
     gray_frame = gray_frame.astype(np.float32)
     gray_frame = gray_frame.flatten()
 
@@ -24,20 +25,21 @@ while(video.isOpened()):
     if cv2.waitKey(1) == ord("q"):
         break
 video.release()
+print(A[0].shape)
 X = np.column_stack(A)
 
 #print("The video is ", len(A), " Frames long")
 
 print(X.shape)
-Q,S,Vt = svds(X.astype(float), k=100)
+Q,S,Vt = svds(X.astype(float), k=10)
 
-L = 99
+L = 10
 Xhat = Q[:, :L] @  np.diag(S[:L]) @ Vt[:L,:]
 
 for i in range(Xhat.shape[1]):
 
     frame = Xhat[:, i]
-    frame = frame.reshape(1028, 1028)
+    frame = frame.reshape(1920, 1080)
 
     frame = cv2.normalize(frame, None, 0, 255, cv2.NORM_MINMAX)
     frame = frame.astype(np.uint8)
