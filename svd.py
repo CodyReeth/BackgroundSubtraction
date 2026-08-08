c411f5679c2be647a5835dbeffe98dc163fa01ec
Cody <cody.reeth@gmail.com>
2026-08-07 21:38:01 -0400

    feat(back): add video foreground reconstruction

--- svd.py
+++ svd.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.sparse.linalg import svds
 import cv2
 
 video = cv2.VideoCapture("video.mov")
@@ -8,12 +11,40 @@ while(video.isOpened()):
     ret, frame = video.read()
     if not ret:
         break
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     cv2.imshow("Video", frame)
-    A.append(frame)
 
+    #gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame = cv2.resize(frame, (1028, 1028))
+    gray_frame = gray_frame.astype(np.float32)
+    gray_frame = gray_frame.flatten()
+
+    A.append(gray_frame)
     if cv2.waitKey(1) == ord("q"):
         break
+video.release()
+X = np.column_stack(A)
 
-print("The video is ", len(A), " Frames long")
+#print("The video is ", len(A), " Frames long")
 
+print(X.shape)
+Q,S,Vt = svds(X.astype(float), k=100)
+
+L = 99
+Xhat = Q[:, :L] @  np.diag(S[:L]) @ Vt[:L,:]
+
+for i in range(Xhat.shape[1]):
+
+    frame = Xhat[:, i]
+    frame = frame.reshape(1028, 1028)
+
+    frame = cv2.normalize(frame, None, 0, 255, cv2.NORM_MINMAX)
+    frame = frame.astype(np.uint8)
+
+    cv2.imshow("Foreground", frame)
+
+    if cv2.waitKey(33) == ord("q"):
+        break
+
+cv2.destroyAllWindows()
